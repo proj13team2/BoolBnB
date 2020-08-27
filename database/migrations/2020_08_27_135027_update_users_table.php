63f0020363ff6af6d22733f97e90bd6f76edee60
Momo Ramadori <momoramadori8@gmail.com>
2020-08-27 16:45:04 +0200

    created role table and updated users table

---
+++ database/migrations/2020_08_27_135027_update_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('lastname', 50)->nullable()->after('name');
+            $table->date('date_of_birth')->nullable()->after('lastname');
+            $table->unsignedBigInteger('role_id')->after('id');
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_role_id_foreign');
+            $table->dropColumn('date_of_birth');
+            $table->dropColumn('lastname');
+        });
+    }
+}

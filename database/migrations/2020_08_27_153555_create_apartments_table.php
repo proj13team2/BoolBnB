fb9fb18da32275da107238eda46fb1c696a78638
pietro <pietro1990@hotmail.it>
2020-08-27 18:23:52 +0200

    tabella appartamenti

---
+++ database/migrations/2020_08_27_153555_create_apartments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('address');
+            $table->float('price', 8, 2);
+            $table->smallInteger('number_of_rooms');
+            $table->smallInteger('number_of_bathrooms');
+            $table->smallInteger('square_meters');
+            $table->string('src')->unique();
+            $table->float('address_lat', 10, 7);
+            $table->float('address_lng', 10, 7);
+            $table->string('slug')->unique();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}

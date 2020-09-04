cff89c8ae03fbee9882b46cee6e3c8af1d269ce2
Edoardo Piergentili <epiergentili@outlook.it>
2020-09-04 18:32:24 +0200

    Mini dettaglio per chiarezza nella lettura Messages

--- resources/views/user/apartments/messages.blade.php
+++ resources/views/user/apartments/messages.blade.php
@@ -4,13 +4,14 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <p></p>
             @foreach ($user_messages as $user_message)
-            <p> Messaggio da {{$user_message->email}} per l'appartamento {{ $user_message->apartment_id }}</p>
-            <p> {{  $user_message->content}}</p>
+              <div class="message">
+                <h4> Messaggio da: {{$user_message->email}}</h4>
+                <h5> Appartamento: {{ $user_message->apartment_id }}</h5>
+                <p> {{  $user_message->content}}</p>
+              </div>
             @endforeach
         </div>
     </div>
 </div>
 @endsection
-

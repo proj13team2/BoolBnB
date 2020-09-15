0a66d9b2dfc349640e28fe000a6741bea509ce17
Edoardo Piergentili <epiergentili@outlook.it>
2020-09-15 19:59:28 +0200

    Grafica html + App cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,79 +25,89 @@
 
 @section('content')
 <div class="container">
-  <div class="FORM_SEARCH float yellow">
-    <div class="form_fallovede disabled">
-      <form class="" action="index.html" method="post">
-        @csrf
-        <div class="services">
-          @foreach ($services as $service)
-              <div class="form-check form-check-inline ">
-                  <label class="form-check-label">
-                      <input class='services_input'
-                      name="service_id[]" class="form-check-input" type="checkbox" value="{{ $service->type }}">
-                      {{ $service->type }}
-                  </label>
-              </div>
-          @endforeach
-        </div>
-        <div class="number_of">
-          <label for="number_of_rooms">Select number of rooms:</label>
-          <select class="" id="number_of_rooms" name="number_of_rooms">
-            <option value="">...</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
-          <label for="number_of_beds">Select number of beds:</label>
-          <select class="" id="number_of_beds" name="number_of_beds">
-            <option value="">...</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
-        </div>
-        <div class="slider">
-          <label for="km_slider">Km (between 0 and 100):</label>
-          <input type="range" min="1" max="100" id="km_slider" name="km_slider" value="100">
-          <p>value: <span id="slider_range"></span></p>
-        </div>
-        <button type="button" id='go' name="button">Go</button>
-      </form>
-    </div>
- </div>
   <p class="brown" id="ricerca_user"></p>
-      <div class="SPONSORIZED d-flex justify-content-center align-items-center blue
-      .">
+      <div class="SPONSORIZED d-flex justify-content-center align-items-center blue.">
       </div>
-      <div class='our_results d-flex justify-content-center align-items-center gray'>
+  <div class="searchedinputresult d-flex ">
+    <div class="FORM_SEARCH  float yellow">
+      <div class="form_fallovede disabled">
+        <form class="" action="index.html" method="post">
+          @csrf
+          <div class="services  d-flex flex-column">
+            @foreach ($services as $service)
+                <div class="form-check form-check-inline ">
+                    <label class="form-check-label">
+                        <input class='services_input'
+                        name="service_id[]" class="form-check-input" type="checkbox" value="{{ $service->type }}">
+                        {{ $service->type }}
+                    </label>
+                </div>
+            @endforeach
+          </div>
+          <div class="number_of">
+            <label for="number_of_rooms">Select number of rooms:</label>
+            <select class="" id="number_of_rooms" name="number_of_rooms">
+              <option value="">...</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+            <label for="number_of_beds">Select number of beds:</label>
+            <select class="" id="number_of_beds" name="number_of_beds">
+              <option value="">...</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+          <div class="slider">
+            <label for="km_slider">Km (between 0 and 100):</label>
+            <input type="range" min="1" max="100" id="km_slider" name="km_slider" value="100">
+            <p>value: <span id="slider_range"></span></p>
+          </div>
+          <button type="button" id='go' name="button">Go</button>
+        </form>
       </div>
+   </div>
+   <div class='our_results d-flex justify-content-center align-items-center flex-wrap gray'>
+   </div>
+  </div>
 </div>
 <script id="our_results" type="text/x-handlebars-template">
-<div class="apartment_result">
-  <img src="storage/@{{src}}" width="200px" height="125px">
-<h4><a  href="@{{link}}"> @{{title}} </a></h4>
-  <p> Address : @{{street}} @{{building_number}} @{{city}} @{{region}}  @{{zip_code}} </p>
+<div class="card apartment_result">
+        <img class="card-img-top" src="storage/@{{src}}" width="200px" height="125px">
+   <div class="card-body">
+       <h4 class="card-title"><a  href="@{{link}}"> @{{title}} </a></h4>
+       <p class="card-text"> Address : @{{street}} @{{building_number}} </p>
+       <p class="card-text">  @{{city}}</p>
+       <a href="@{{link}}" class="btn btn-primary">Go Visit</a>
+  </div>
 </div>
 </script>
 <script id="SPONSORIZED" type="text/x-handlebars-template">
-  <div class="apartment_sponsorized yellow">
-    <img src="storage/@{{src}}" width="200px" height="125px">
-  <h4><a  href="@{{link}}"> @{{title}} </a></h4>
-    <p> Address : @{{street}} @{{building_number}} @{{city}} @{{region}}  @{{zip_code}} </p>
+  <div class="card apartment_result">
+          <img class="card-img-top" src="storage/@{{src}}" width="200px" height="125px">
+     <div class="card-body">
+         <h4 class="card-title"><a  href="@{{link}}"> @{{title}} </a></h4>
+         <p class="card-text"> Address : @{{street}} @{{building_number}}</p>
+         <p class="card-text"> @{{city}}</p>
+         <a href="@{{link}}" class="btn btn-primary">Go Visit</a>
+    </div>
+  </div>
 </script>
 @endsection
 @push('head')

9bc65bde6cda1cfe004441692e2fab2f3bf441d3
Edoardo Piergentili <epiergentili@outlook.it>
2020-09-20 17:45:44 +0200

    Sponsorization

--- resources/views/user/apartments/sponsorization.blade.php
+++ resources/views/user/apartments/sponsorization.blade.php
@@ -9,60 +9,61 @@
         <h1>L'appartamento è sponsorizzato</h1>
     @else
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h4> Seleziona la stanza "{{$apartment->title}}"</h4>
-                <p>Seleziona la modalità di sponsorizzazione:</p>
+        <h1 class="text-center "> Sponsorization for: <span class="green-text">{{$apartment->title}}</span></h1>
+      <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        <h1 class="display-4 green-text">Sponsor</h1>
+        <p class="lead text-muted">The best and quick way to get attention! Let your insertion appear everywhere on our site for a small amount of price. The quickest way to get the attention your apartments needs.</p>
+      </div>
 
-                @if (session('success_message'))
-                <div class="alert alert-success">
-                    {{ session('success_message') }}
-                </div>
-                @endif
+                      @if (session('success_message'))
+                      <div class="alert alert-success">
+                          {{ session('success_message') }}
+                      </div>
+                      @endif
 
-                @if (count($errors) > 0)
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                        <li> {{ $error }} </li>
-                        @endforeach
-                    </ul>
-                </div>
-                @endif
+                      @if (count($errors) > 0)
+                      <div class="alert alert-danger">
+                          <ul>
+                              @foreach ($errors->all() as $error)
+                              <li> {{ $error }} </li>
+                              @endforeach
+                          </ul>
+                      </div>
+                      @endif
 
-                <form method="post" id="payment-form" action="{{route('user.apartment.sponsorization', ['apartment' => $apartment->id])}}">
+      <div class="card-deck mb-3  text-center">
+
+                <form method="post" class="row col-12"id="payment-form" action="{{route('user.apartment.sponsorization', ['apartment' => $apartment->id])}}">
                     @csrf
-                    <section>
-                    <div class="form-group">
                         @foreach ($sponsors as $sponsor)
-                        <div class="form-check">
+                          <div class="card mb-4 shadow-sm">
+                          <div class="card-header">
+                            <h4 class="my-0 font-weight-normal">{{$sponsor->sponsorship_level}}</h4>
+                          </div>
+                            <div class="card-body text-center">
+                                <h1 class="card-title pricing-card-title">${{ $sponsor->price }} </h1>
+                                <ul class="list-unstyled mt-3 mb-4">
+                                  <li class="text-muted">Show Apartment in Homepage</li>
+                                  <li class="text-muted">Carousel show</li>
+                                  <li><strong>Duration</strong></li>
+                                  <li><span class="font-big pt-3"><strong>{{($sponsor->duration) * 24}}</strong></span><small class="text-muted">/ hr</small></li>
+                                </ul>
                             <label for="amount" class="form-check-label">
-                                <input id="amount" name="amount" class="form-check-input sponsorship-level" type="radio"  value=" {{ $sponsor->id }}" checked>
-                                {{ $sponsor->price }} € per {{($sponsor->duration) * 24}} ore di sponsorizzazione
+                                <input id="amount" name="amount" class="form-check-input sponsorship-level" type="radio"  value=" {{ $sponsor->id }} " checked>
                             </label>
                         </div>
+                        </div>
                         @endforeach
-                    </div>
-
-                        <div class="bt-drop-in-wrapper">
+                        <div class="bt-drop-in-wrapper col-12">
                             <div id="bt-dropin"></div>
                         </div>
-                    </section>
-                    <input id="nonce" name="payment_method_nonce" type="hidden" />
-                    <button class="button" type="submit"><span>Proceed to payment</span></button>
-                    {{-- @foreach ($apartment->sponsors as $sponsor)
-                    @if( $active == 0)
-                        <button class="button" type="submit"><span>Proceed to payment</span></button>
-                    @else
-                        <button class="button" type="submit" disabled><span>Proceed to payment</span></button>
-                    @endif
-                    @endforeach --}}
-                </form>
-            </div>
-        </div>
-    </div>
+                        <div class="col-12">
+                          <input id="nonce" name="payment_method_nonce" type="hidden" />
+                          <button class="button btn btn-green-moon white-text" type="submit"><span>Proceed to payment</span></button>
+                        </div>
+                  </form>
+      </div>
     @endif
-
 
 <script src="https://js.braintreegateway.com/web/dropin/1.23.0/js/dropin.min.js"></script>
 <script>
@@ -72,10 +73,8 @@
 
     braintree.dropin.create({
         authorization: client_token,
-        selector: '#bt-dropin',
-        paypal: {
-            flow: 'vault'
-        }
+        selector: '#bt-dropin'
+
     }, function(createErr, instance) {
         if (createErr) {
             console.log('Create Error', createErr);
